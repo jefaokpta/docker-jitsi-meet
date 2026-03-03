532dd3a54a6621a29d6a10cabde71e60e92ded20
Jefferson Alves <jefaokpta@hotmail.com>
2026-03-03 09:42:58 -0300

    controle refinado de recursos - Add custom interface configuration for app settings

---
+++ jefao-files/custom-interface_config.js
@@ -0,0 +1,10 @@
+ //CUSTOM CONFIG
+interfaceConfig.APP_NAME = 'Vip Video Conferencia';
+interfaceConfig.DISPLAY_WELCOME_FOOTER = false;
+interfaceConfig.GENERATE_ROOMNAMES_ON_WELCOME_PAGE = false;
+interfaceConfig.SETTINGS_SECTIONS = [ 'devices', 'language', 'moderator', 'profile', 'calendar', 'sounds']
+interfaceConfig.SHOW_JITSI_WATERMARK = false;
+interfaceConfig.HIDE_DEEP_LINKING_LOGO = true;
+interfaceConfig.NATIVE_APP_NAME = 'Vip Video Conferencia';
+interfaceConfig.MOBILE_DOWNLOAD_LINK_IOS = 'https://apps.apple.com/br/app/vip-video-chat/id1523091897';
+interfaceConfig.MOBILE_DOWNLOAD_LINK_ANDROID = 'https://play.google.com/store/apps/details?id=br.com.vip.vipvideochat';
